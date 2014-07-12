b27f017c04165815cd457d9bf021dd3fe3425266
Kitty cat <kremlin9@mail.com>
2014-07-12 17:07:28 +0400

    fix

--- src/Event/GeneralBundle/Entity/InternalEvent.php
+++ src/Event/GeneralBundle/Entity/InternalEvent.php
@@ -729,4 +729,19 @@ class InternalEvent
             return $this->name;
         }
     }
+
+    private function tokenTruncate($string, $your_desired_width) {
+        $parts = preg_split('/([\s\n\r]+)/u', $string, null, PREG_SPLIT_DELIM_CAPTURE);
+        $parts_count = count($parts);
+
+        $length = 0;
+        $last_part = 0;
+        for (; $last_part < $parts_count; ++$last_part) {
+            $length += mb_strlen($parts[$last_part]);
+            if ($length > $your_desired_width) { break; }
+        }
+
+        return implode(array_slice($parts, 0, $last_part));
+    }
+
 }
